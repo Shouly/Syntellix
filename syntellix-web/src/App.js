03bf2723965e575252773e2bb8bb5a678569ef9f
liangbing <bing.liang@relxintl.com>
2024-09-08 01:15:44 +0800

    update.

--- syntellix-web/src/App.js
+++ syntellix-web/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
+import AuthForm from './components/AuthForm';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Hello React + Tailwind!
-      </h1>
+    <div className="App">
+      <AuthForm />
     </div>
   );
 }
